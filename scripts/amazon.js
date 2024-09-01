6aa759c558d7fefa39f8cd9cc42e8200c4faffc9
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-09-01 12:51:45 +0530

    checkout page, made interactive delete button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import {cart, addToCart} from '../data/cart.js';
+import { products } from '../data/products.js';
+
 let productsHTML='';
 products.forEach((product)=>{
     const html=`
@@ -55,31 +58,26 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
+
+function updateCartQuantity(){
+    let cartQuantity=0;
+        
+    cart.forEach((cartItem)=>{
+        cartQuantity+=cartItem.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+    
+    console.log(cart);
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click', ()=>{
         const productId=button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
 
-        let matchingItem='';
-        cart.forEach((item)=>{
-            if(productId===item.productId) matchingItem=item;
-        })
-        if(matchingItem) matchingItem.quantity++;
-        else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
-
-        let cartQuantity=0;
-        
-        cart.forEach((item)=>{
-            cartQuantity+=item.quantity;
-        });
-        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
-
-        
-        console.log(cart);
+       
     });
     
 });
